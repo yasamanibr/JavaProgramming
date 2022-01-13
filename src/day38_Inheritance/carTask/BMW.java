2b989924bf954c2c39308dc6d270338f4dda99e5
yasaman <yasamanibr@gmail.com>
2022-01-13 10:57:34 -0500

    day38_ContinueInheritance

---
+++ src/day38_Inheritance/carTask/BMW.java
@@ -0,0 +1,14 @@
+package day38_Inheritance.carTask;
+
+public class BMW extends Car{
+    public BMW(String model, int year, double price, String color, int miles) {
+        super("BMW", model, year, price, color, miles);
+    }
+    public void breaksDown(){
+
+    }
+
+    public void racing(){
+
+    }
+}
--- src/day38_Inheritance/carTask/Toyota.java
+++ src/day38_Inheritance/carTask/Toyota.java
@@ -1,11 +1,12 @@
 package day38_Inheritance.carTask;
 
 public class Toyota extends Car{
-    public Toyota(String model, int year, double price, String color, double miles) {
+    public Toyota(String model, int year, double price, String color, int miles) {
         super("Toyota", model, year, price, color, miles);
     }
 
     public void reliable(){
+        System.out.println(brand + " " + model + " is reliable");
 
     }
 }
